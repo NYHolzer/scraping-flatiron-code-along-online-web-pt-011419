e85ba37a16e371ab55efab6054c3927d278e12c9
Nissan Holzer <nissan.holzer@gmail.com>
2019-03-01 00:24:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,18 +16,18 @@ class Scraper
       end
     end
   end
-  
+
   def get_page
+ 
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-    # doc.css(".post").each do |post|
-    #   course = Course.new 
-    #   course.title = post.css("h2").text 
-    #   course.schedule = post.css(".date").text 
-    #   course.description = post.css("p").text 
-    # end
-    end
-  end 
+ 
+    #doc.css(".post").each do |post|
+      #course = Course.new
+      #course.title = post.css("h2").text
+      #course.schedule = post.css(".date").text
+      #course.description = post.css("p").text
+    #end
+  end
   
   def 
   
